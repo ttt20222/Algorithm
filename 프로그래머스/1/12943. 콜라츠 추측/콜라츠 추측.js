cf84ef1524de1357752e31da6757c2ae889644e5
jelee <132965634+ttt20222@users.noreply.github.com>
2024-05-02 11:14:47 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,8 @@
+function solution(num) {
+    let answer = 0;
+    for(let i=0 ; i<500 ; i++){
+        if(num != 1){num = num%2 === 0 ? num/2 : (num*3)+1;}
+        else{return i}
+    }
+    return -1;
+}
