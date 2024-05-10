95e90acc3862be10ddefefd26df3254473ea03fc
jelee <132965634+ttt20222@users.noreply.github.com>
2024-05-10 16:43:14 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -7,11 +7,7 @@ function solution(s) {
 
     for(let i=0; i<arr.length; i++){
         for(let j=0; j<arr[i].length; j++){
-            if(j%2 === 0){//짝수
-                arr[i][j] = arr[i][j].toUpperCase();
-            }else{//홀수
-                arr[i][j] = arr[i][j].toLowerCase();
-            }
+            arr[i][j] = j%2 === 0 ? arr[i][j].toUpperCase() : arr[i][j].toLowerCase();
         }
     }
     
