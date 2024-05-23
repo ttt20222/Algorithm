7236728ff1e29dbc549063bf7200bb29a88c1c2f
jelee <132965634+ttt20222@users.noreply.github.com>
2024-05-23 15:17:15 +0900

    [level 1] Title: 과일 장수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,10 @@
+function solution(k, m, score) {
+    let answer = 0;
+    
+    score.sort((a,b) => b-a);
+    
+    for(i=m; i <= score.length ; i = i+m){
+        answer += score[i-1]*m;
+    }
+    return answer;
+}
