f20ae6723b0c2eec274b92969582642d5d9b433e
jelee <132965634+ttt20222@users.noreply.github.com>
2024-06-20 10:04:15 +0900

    [level 2] Title: 프로세스, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -0,0 +1,18 @@
+function solution(priorities, location) {
+  let answer = 0;
+  const array = priorities.map((process,index) => {
+    return {process, index};
+  })
+
+  while(array.length){
+    const queue = array.shift();
+
+    if(array.some((element) => element.process > queue.process)){
+      array.push(queue);
+    }else{
+      answer++
+      if(queue.index === location) break;
+    }
+  }
+  return answer;
+}
