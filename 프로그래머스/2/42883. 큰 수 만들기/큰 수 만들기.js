c3baf80feae7578dad5145ce9b77903528167c4a
jelee <132965634+ttt20222@users.noreply.github.com>
2024-07-05 17:20:42 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -0,0 +1,14 @@
+function solution(number, k) {
+    let stack = [];
+    
+    for(let i=0; i<number.length; i++) {
+        const element = number[i]
+        while(k > 0 && stack[stack.length-1] < element) {
+            stack.pop();
+            k--;
+        }
+        stack.push(element);
+    }
+    stack.splice(stack.length - k, k);
+    return stack.join('');
+}
