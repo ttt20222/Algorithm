efb5c37513cc8c5a88d1f15b52ce32ed35582d6d
jelee <132965634+ttt20222@users.noreply.github.com>
2024-10-13 21:15:49 +0900

    [level 2] Title: 가장 큰 수, Time: 161.11 ms, Memory: 44.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -3,5 +3,5 @@ function solution(numbers) {
         .map(numbers => String(numbers))
         .sort((a,b) => (b+a) - (a+b))
         .join('');
-    return answer[0] === '0' ? '0' : answer;;
+    return answer[0] === '0' ? '0' : answer;
 }
